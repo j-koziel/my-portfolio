ea299389b0e61efe7266d9b63ae623fc7b61b9de
j-koziel <jonathankoziel1@gmail.com>
2024-08-28 14:41:09 +0100

    feat: created the projects section and wrote some tests

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -1,10 +1,55 @@
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
 export function ProjectsSection() {
+  const projectsData = [
+    {
+      title: "Riget Zoo Adventures",
+      description:
+        "Experience wildlife like never before with this interactive experience",
+      img: "/rza_ss.jpg",
+      img_alt: "A screenshot of the Riget Zoo Adventures app",
+    },
+    {
+      title: "Health Advice",
+      description: "Health information based on the weather in your location",
+      img: "/ha_ss.jpg",
+      img_alt: "A screenshot of the health advice app",
+    },
+  ];
+
   return (
     <main
       id="projects"
-      className="min-h-screen flex items-center justify-center"
+      className="min-h-screen flex items-center justify-evenly"
     >
-      Projects coming soon
+      <div>
+        <h1 className="text-8xl font-bold">Projects</h1>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        {projectsData.map((project, i) => (
+          <Card key={i}>
+            <CardHeader>
+              <CardTitle>{project.title}</CardTitle>
+              <CardDescription>{project.description}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex items-center justify-center">
+              <Image
+                src={project.img}
+                height={200}
+                width={200}
+                alt={project.img_alt}
+              />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
